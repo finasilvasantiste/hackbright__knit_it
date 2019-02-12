45bf05bed7a52e75a6b50b090832a9d765fda5af
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-12 15:54:16 -0800

    Added class http_handler to handle http and authentication. Added data model diagram.

---
+++ http_handler.py
@@ -0,0 +1,53 @@
+### HTTP requests are handled here. ###
+
+import requests
+import json
+
+class HTTP_Handler():
+
+    BASE_URL = 'https://api.ravelry.com/'
+    AUTH = ''
+
+    def set_auth(self):
+        with open('config.json', 'r') as f:
+            config = json.load(f)
+
+        USER = config['DEFAULT']['USER_NAME']
+        PASSWORD = config['DEFAULT']['PASSWORD']
+        self.AUTH = (USER, PASSWORD)
+
+
+    def __init__(self):
+        self.set_auth()
+
+
+
+    def submit_post(self, URL, dictToSend):
+        """Returns response of external api as json."""
+
+        try: 
+            response = requests.post(URL, json=dictToSend)
+            return response.json()
+        except:
+            return {"status" : "500" , "reason" : "Post unsuccessful."}
+
+
+    def submit_get(self, URL):
+        """Returns response of external api as json."""
+        try: 
+            response = requests.get(URL, auth=self.AUTH)
+            return response.json()
+        except:
+            return {"status" : "500" , "reason" : "Post unsuccessful."}
+
+
+    def compose_url(self, pre_url, p_id):
+        pattern_id = p_id
+        prefix_url = pre_url
+        suffix_url = '.json'
+
+        partial_url = '{}{}{}'.format(prefix_url, pattern_id, suffix_url)
+
+        url = '{}{}'.format(self.BASE_URL, partial_url)
+        
+        return url
--- ravelry_handler.py
+++ ravelry_handler.py
@@ -1,75 +1,19 @@
-### All calls to Ravelry Api are handled here. ###
+### Calls to Ravelry Api are handled here. ###
 
-import requests
-import json
+from http_handler import HTTP_Handler
+
 
 class Ravelry_handler():
 
-    BASE_URL = 'https://api.ravelry.com/'
-    AUTH = ''
+    HTTP_HANDLER = HTTP_Handler()
 
 
-
-    def set_auth(self):
-        with open('config.json', 'r') as f:
-            config = json.load(f)
-
-        USER = config['DEFAULT']['USER_NAME']
-        PASSWORD = config['DEFAULT']['PASSWORD']
-        self.AUTH = (USER, PASSWORD)
-
-
-    def __init__(self):
-        self.set_auth()
-
-
-
-
-    def submit_post(self, URL, dictToSend):
-        """Returns response of external api as json."""
-
-        try: 
-            response = requests.post(URL, json=dictToSend)
-            return response.json()
-        except:
-            return {"status" : "500" , "reason" : "Post unsuccessful."}
-
-
-    def submit_get(self, URL):
-        """Returns response of external api as json."""
-        try: 
-            response = requests.get(URL, auth=self.AUTH)
-            return response.json()
-        except:
-            return {"status" : "500" , "reason" : "Post unsuccessful."}
-
-
-    def compose_url(self, pre_url, p_id):
-        pattern_id = p_id
-        prefix_url = pre_url
-        suffix_url = '.json'
-
-        partial_url = '{}{}{}'.format(prefix_url, pattern_id, suffix_url)
-
-        url = '{}{}'.format(self.BASE_URL, partial_url)
-
-        print('++++++++')
-        print(url)
-        return url
 
     def get_pattern(self, p_id):
         
-        url = self.compose_url('patterns/', p_id)
+        url = self.HTTP_HANDLER.compose_url('patterns/', p_id)
 
-        resp_from_server = self.submit_get(url)
+        resp_from_server = self.HTTP_HANDLER.submit_get(url)
 
         return resp_from_server
     
-
-
-
-
-
-
-
-
