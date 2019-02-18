04e4ee638269d3bb9353fb22211d30b474b84a8d
finasilvasantiste <fina.santiste@gmail.com>
2019-02-18 15:57:41 -0800

    Added button to go to previous minipatterns results page.

--- static/js/MiniPatterns.jsx
+++ static/js/MiniPatterns.jsx
@@ -24,6 +24,7 @@ export default class MiniPatterns extends React.Component {
 
         this.getPatternID = this.getPatternID.bind(this);
         this.getNextPage = this.getNextPage.bind(this);
+        this.getPreviousPage = this.getPreviousPage.bind(this);
     }
 
 
@@ -39,17 +40,10 @@ export default class MiniPatterns extends React.Component {
     }
 
 
-    getPythonMiniPatterns(increment){
+    getPythonMiniPatterns(next_page){
         /* 
             Gets mini patterns from python server and forwards it to set the new state of component.
         */
-        let next_page
-
-        if (increment){
-            next_page = this.state.page_number + 1
-        }else{
-            next_page = this.state.page_number
-        };
 
         const route = '/patterns/knitting/page/'
 
@@ -64,6 +58,29 @@ export default class MiniPatterns extends React.Component {
         });
     }
 
+    setPage(increment, decrement){
+        /*
+            Sets page number to use for api call. 
+            If current page is first page and user tries to get previous page,
+            no call is executed.
+        */
+
+        if (increment){
+            this.getPythonMiniPatterns(this.state.page_number + 1)
+        }else{
+            if(decrement){
+                if (this.state.page_number > 1){
+                    this.getPythonMiniPatterns(this.state.page_number - 1)
+                }else{
+                    console.log('No previous page! Already on page '+ this.state.page_number)
+                }
+            }else{
+                this.getPythonMiniPatterns(this.state.page_number)
+            }
+        };
+
+    }
+
     getPatternID(pattern_id){
         console.log('Click!')
         console.log(pattern_id)
@@ -75,11 +92,16 @@ export default class MiniPatterns extends React.Component {
     };
 
     getNextPage(){
-        this.getPythonMiniPatterns(true); 
+        this.setPage(true, false); 
+    }
+
+
+    getPreviousPage(){
+        this.setPage(false, true); 
     }
 
     componentDidMount() {
-        this.getPythonMiniPatterns(false); 
+        this.setPage(false, false); 
     }
 
     render(){
@@ -88,6 +110,14 @@ export default class MiniPatterns extends React.Component {
                 <Col md={7} >
                     <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam varius efficitur nulla ut condimentum. Phasellus luctus lacinia nisi, nec porta neque placerat vitae. In sed gravida metus. Donec dolor felis, ultrices in lacus sit amet, posuere laoreet dolor. Mauris rhoncus mauris ac tellus finibus, cursus tincidunt lectus rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin imperdiet sem quam, vitae molestie ipsum blandit quis. Donec viverra eros vitae augue euismod elementum. Suspendisse rhoncus massa vitae dolor dapibus, sit amet feugiat mauris rhoncus. Nam sapien felis, sagittis at sapien quis, vulputate vehicula ligula. Sed ac egestas justo, at fringilla est. Nam eleifend, nisl vitae maximus sagittis, felis massa dapibus elit, in pharetra justo ex non eros.
+                    </p>
+                    <p>
+                        <Button bsSize="large" bsStyle="danger" onClick={this.getNextPage}>
+                          Next Page!
+                        </Button>
+                        <Button bsSize="large" bsStyle="danger" onClick={this.getPreviousPage}>
+                          Previous Page!
+                        </Button>
                     </p>
                     {this.state.data.map(d => 
                         <div key={d.name} onClick={()=>this.getPatternID(d.pattern_id)}>
@@ -102,9 +132,6 @@ export default class MiniPatterns extends React.Component {
                 <Col md={5}>
                     <Pattern id={this.state.pattern_id}/> 
                 </Col> 
-                <Button bsSize="large" bsStyle="danger" onClick={this.getNextPage}>
-                  Next Page!
-                </Button>
             </Row>
             );
     }
